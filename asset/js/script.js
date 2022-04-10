e49ea1129aac969462c4d0f1f76f5c8a3614e980
farrelfay21 <farrelfay.251@gmail.com>
2022-04-10 19:56:21 +0700

    git push -u origin main

---
+++ asset/js/script.js
@@ -0,0 +1,18 @@
+window.onresize = function reportWindowSize() {
+	let title = "";
+
+	if (window.matchMedia("(min-width: 1536px)").matches) {
+		title += " 2xl | ";
+	} else if (window.matchMedia("(min-width: 1280px)").matches) {
+		title += "xl | ";
+	} else if (window.matchMedia("(min-width: 1024px)").matches) {
+		title += " lg | ";
+	} else if (window.matchMedia("(min-width: 768px)").matches) {
+		title += " md | ";
+	} else if (window.matchMedia("(min-width: 640px)").matches) {
+		title += "sm | ";
+	}
+	title += "My Portfolio";
+
+	document.querySelector("title").text = title;
+};
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+	content: ["index.html"],
+	theme: {
+		container: {
+			center: true,
+			padding: "16px",
+		},
+		extend: {
+			colors: {
+				primary: "#8b5cf6",
+				"primary-variant": "#46244C",
+				secondary: "#C74B50",
+				accent: "#D49B54",
+			},
+			screens: {
+				"2xl": "1320px",
+			},
+		},
+	},
+	plugins: [],
+};
